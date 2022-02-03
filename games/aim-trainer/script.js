23d2528ebd48608f6295f0a661dda5a5d3f965f8
quasar098 <patellmlg@gmail.com>
2022-02-03 10:00:11 -0500

    added moving of the targets

--- games/aim-trainer/script.js
+++ games/aim-trainer/script.js
@@ -1,8 +1,22 @@
 const restartButton = document.getElementById("restart");
-var bruh = [];
+var targets = [];
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function relocate(_) {
+  _.style.top = getRandomArbitrary(10, 80) + "%";
+  _.style.left = getRandomArbitrary(10, 90) + "%";
+}
 
 for (let addTarget of document.getElementsByClassName("target")) {
-  console.log(addTarget);
+  addTarget.addEventListener("mousedown", (event) => {
+    if (event.button == 0) {
+      relocate(addTarget);
+    }
+  });
+  targets.push(addTarget);
 }
 
 
@@ -13,5 +27,8 @@ restartButton.addEventListener("mousedown", (event) => {
 });
 
 function restart() {
-  console.log("do this");
+  for (let target of targets) {
+    relocate(target);
+  }
 }
+restart();
