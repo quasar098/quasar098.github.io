062309d32e2d0a8711723e96987bbc93d2724d69
quasar098 <patellmlg@gmail.com>
2022-02-13 17:13:05 -0500

    repeating timer is finished

---
+++ games/repeat-timer/script.js
@@ -0,0 +1,37 @@
+const timerText = document.getElementById("main");
+const statsText = document.getElementById("total-stats");
+let prevTimer = 0;
+let running = false;
+let baseTime = 40;
+let time = baseTime;
+let beep = new Audio("beep.wav");
+timerText.innerHTML = time;
+
+
+function timer() {
+	time -= 1;
+	if (time == 1) {
+		setTimeout(() => {
+					beep.play();
+		}, 600);
+
+
+	}
+	if (time == 0) {
+		time = baseTime;
+
+	}
+	timerText.innerHTML = time;
+}
+
+
+document.addEventListener("pointerdown", (e) => {  // make it work with mobile devices
+	running = !running;
+	if (running) {
+		prevTimer = setInterval(timer, 1000);
+		statsText.innerHTML = "running";
+	} else {
+		clearInterval(prevTimer);
+		statsText.innerHTML = "paused";
+	}
+});
