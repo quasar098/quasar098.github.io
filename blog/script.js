d41473b5314e6fd94b0433cd61d22bd74c3935eb
quasar098 <patellmlg@gmail.com>
2022-05-19 14:23:30 -0400

    amogus

--- blog/script.js
+++ blog/script.js
@@ -29,6 +29,7 @@ function addBlogPost(title, messages, date) {
   blogPostsDiv.appendChild(blogPost);
 }
 
+addBlogPost("mohammad ale", ["the punch", "the kick", "skrunkly the when"], "05-19-2022");
 addBlogPost("just super writing here", ["powerlanguage", "divides", "powergrammar", "powervocabulary", "powersentencestructure", "I have the power"], "05-16-2022");
 addBlogPost("reporting 7", ["yet again at the table", "circle table", "people walk past me", "pasta me", "pasta"], "05-13-2022");
 addBlogPost("surveillance", ["there is always someone watching you"], "05-12-2022");
