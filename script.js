a9e156da6f82de5f113da1fa6b8348ce50d27769
quasar098 <patellmlg@gmail.com>
2022-10-10 15:33:53 -0400

    new and improved

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(123);
+const inner = document.getElementById('inner');
+const left = document.getElementById('sleft');
+const right = document.getElementById('sright');
+
+setInterval(() => {
+    left.style.display = inner.scrollLeft != 0 ? "flex" : 'none';
+    right.style.display = inner.scrollWidth-inner.getBoundingClientRect().width != inner.scrollLeft ? 'flex' : 'none';
+    inner.scrollLeft = inner.scrollLeft*0.95+goalScroll*0.05;
+}, 5)
+
+goalScroll = 0;
+
+right.addEventListener("click", (e) => {
+    goalScroll += 100;
+});
+left.addEventListener("click", (e) => {
+    goalScroll -= 100;
+});
